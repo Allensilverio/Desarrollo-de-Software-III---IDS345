84184bc2c79656430b3db922eb1b8c37fe07cca9
Allen <1104220@est.intec.edu.do>
2023-02-15 11:12:21 -0400

    Creación del proyecto

---
+++ Cliente con Transacciones/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente_con_Transacciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
